b37b14c863f3817b592fb9043321d32556e80d70
amlane <twelvexstring@gmail.com>
2019-04-09 17:43:47 -0500

    added contact page and created addition file contact.js

--- contact.js
+++ contact.js
@@ -1,5 +1,4 @@
 
-
 const mainHeader = document.querySelector('h1');
 mainHeader.addEventListener('click', (event) => {
     event.target.style.textTransform = "uppercase";
@@ -28,48 +27,12 @@ funBus.addEventListener('wheel', (event) => {
     event.target.setAttribute ("src", "img/fun-bus.jpg");
 })
 
-const subscribe = document.querySelector('#email');
-subscribe.addEventListener('keydown', (event) => {
-    console.log(`${event.key}`);
-})
+const textArea = document.querySelector('.text-area');
 
-subscribe.addEventListener('focus', (event) => {
-    event.target.style.background = "yellow";
-}, true);
 
-subscribe.addEventListener('blur', (event) => {
-    event.target.style.background = '';    
-  }, true);
 
-const submitEmailBtn = document.querySelector('.subscribe');
-submitEmailBtn.addEventListener('click', (event) => {
-    alert(`WARNING! ALL YOUR EMAIL ARE BELONG TO US`)
-})
 
-const signMeUp = document.querySelector(".content-pick");
-signMeUp.addEventListener('click', (event) => {
-    if (event.target.id === "btn-1"){
-        alert(`Are you sure you want fun in the sun?`);
-        }
-    else if (event.target.id === "btn-2") {
-        alert(`Are you sure you want to climb a mountain?`);
-        }
-    else {
-        alert(`Are you sure you want to stay on an island?`);
-    }
+const sendBtn = document.querySelector('button');
+sendBtn.addEventListener('click', (event) => {
+    alert(`Your message has been sent!`);
 });
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
