07f82d1ff5405dab09e292e4cb0bafc255f056f3
amlane <twelvexstring@gmail.com>
2019-04-09 15:59:12 -0500

    added event listener for dark mode, still needs to be debugged due to requires 3 clicks to make entire page dark mode

--- js/index.js
+++ js/index.js
@@ -1,10 +1,17 @@
 // Your code goes here
 
+const darkMode = document.querySelector('body');
+darkMode.addEventListener('click', (event) => {
+    event.target.style.background = "black";
+    event.target.style.color = "white";
+})
 
 const mainHeader = document.querySelector('h1');
 mainHeader.addEventListener('click', (event) => {
+    event.target.style.textTransform = "uppercase";
     event.target.style.fontWeight = "bold";
-    setTimeout(function(){event.target.style.fontWeight = ""}, 500);
+    setTimeout(function(){event.target.style.textTransform = ""}, 1000);
+    setTimeout(function(){event.target.style.fontWeight = ""}, 1000);
 }) 
 
 const navLinks = document.querySelector('.nav');
@@ -19,9 +26,18 @@ const funBus = document.querySelector('.fun-bus-img');
 funBus.addEventListener('dblclick', (event) => {
     event.target.setAttribute ("src", "img/funbus.png");
 })
+
 funBus.addEventListener('wheel', (event) => {
     event.target.setAttribute ("src", "img/fun-bus.jpg");
 })
+
+
+
+
+
+
+
+
 
 
 
