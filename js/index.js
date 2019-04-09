2a6e16cdda812d2d329436ca34b5becce451fb32
amlane <twelvexstring@gmail.com>
2019-04-09 14:20:20 -0500

    created first eventListener that will create a text shadow when you hover over each nav-link

--- js/index.js
+++ js/index.js
@@ -1,1 +1,16 @@
 // Your code goes here
+
+// let sampleButton = document.querySelector('.sampleButton');
+// sampleButton.addEventListener('click', (event) => {
+//   alert('You Clicked the Sample Button!')
+// setTimeout(function() {
+//     event.target.style.color = "";
+//   }, 500);
+// })
+
+let navLinks = document.querySelector('nav');
+navLinks.addEventListener('mouseover', (event) => {
+    event.target.style.textShadow = "0 0 10px orange";
+    setTimeout(function(){event.target.style.textShadow = "none"}, 300);
+    event.stopPropagation()
+})
