67004b0777a7b7392a505eb107a4519cea25adab
amlane <twelvexstring@gmail.com>
2019-04-09 15:28:05 -0500

    set event listener to change image to make it appear as if background is disappearing

--- js/index.js
+++ js/index.js
@@ -1,16 +1,28 @@
 // Your code goes here
 
-// let sampleButton = document.querySelector('.sampleButton');
-// sampleButton.addEventListener('click', (event) => {
-//   alert('You Clicked the Sample Button!')
-// setTimeout(function() {
-//     event.target.style.color = "";
-//   }, 500);
-// })
 
-let navLinks = document.querySelector('nav');
+const mainHeader = document.querySelector('h1');
+mainHeader.addEventListener('click', (event) => {
+    event.target.style.fontWeight = "bold";
+    setTimeout(function(){event.target.style.fontWeight = ""}, 500);
+}) 
+
+const navLinks = document.querySelector('.nav');
 navLinks.addEventListener('mouseover', (event) => {
-    event.target.style.textShadow = "0 0 10px orange";
-    setTimeout(function(){event.target.style.textShadow = "none"}, 300);
-    event.stopPropagation()
+    if (event.target.tagName === "A") {
+    event.target.style.textShadow = "0 0 10px darkslategrey";
+    setTimeout(function(){event.target.style.textShadow = "none"}, 500);
+    }
 })
+
+const funBus = document.querySelector('.fun-bus-img');
+funBus.addEventListener('dblclick', (event) => {
+    event.target.setAttribute ("src", "img/funbus.png");
+})
+funBus.addEventListener('wheel', (event) => {
+    event.target.setAttribute ("src", "img/fun-bus.jpg");
+})
+
+
+
+
